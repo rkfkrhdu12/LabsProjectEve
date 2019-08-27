4631998130df3d5c9f1e131c9d7745d98125c154
rkfkrhdu12 <33976974+rkfkrhdu12@users.noreply.github.com>
2019-08-27 14:39:03 +0900

    08-27-02:38

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -11,22 +11,49 @@ public class CameraManager : MonoBehaviour
     private void Start()
     {
         player = GameManager.Instance.player.transform;
-
-        isCol = false;
     }
-
-    bool isCol = false;
 
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, player.position, moveSpeed * 2 * Time.deltaTime);
+        Transform parentTrn = transform.parent;
+
+        parentTrn.position = Vector3.Lerp(parentTrn.position, player.position, moveSpeed * 2 * Time.deltaTime);
     }
 
-    void OntriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Wall"))
+        if (other.gameObject.CompareTag("Wall"))
         {
-            Debug.Log(1);
+            Debug.Log(other.name);
+            Material mat = other.GetComponent<MeshRenderer>().material;
+
+            mat.SetFloat("_Mode", 3);
+            mat.color = Color.clear;
+            //mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            //mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            //mat.SetInt("_ZWrite", 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.DisableKeyword("_ALPHABLEND_ON");
+            mat.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = 3000;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log(other.name);
+            Material mat = other.GetComponent<MeshRenderer>().material;
+
+            mat.SetFloat("_Mode", 0);
+            mat.color = Color.white;
+            //mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            //mat.SetInt("_ZWrite", 1);
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.renderQueue = 3000;
         }
     }
 }
